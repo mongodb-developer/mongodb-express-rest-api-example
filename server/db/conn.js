a47a2539471f970b55119b8d768f40bc0653c3e4
Pash10g <pash10g@gmail.com>
2021-08-11 15:35:08 +0300

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -10,15 +10,7 @@ var _db;
 
 module.exports = {
   connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("sample_airbnb");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
+    // Implement Database connection
   },
 
   getDb: function () {
