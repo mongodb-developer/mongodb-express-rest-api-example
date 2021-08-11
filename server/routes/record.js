5d5dc4f061bd0562fe099e2c5c1345cfb38077e1
Pash10g <pash10g@gmail.com>
2021-08-11 15:42:30 +0300

    Update record.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -10,70 +10,22 @@ const dbo = require("../db/conn");
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/listings").get(async function (req, res) {
-  let db_connect = dbo.getDb("sample_airbnb");
-  console.log(req.body.id);
-  db_connect
-    .collection("listingsAndReviews")
-    .find({}).limit(50)
-    .toArray(function (err, result) {
-      if (err)  { res.status(400); 
-                  res.send("Error fetching lisings!"); }
-      else
-      {
-        
-        res.json(result);
-      }
-    });
+ // Get records
 });
 
 // This section will help you create a new record.
 recordRoutes.route("/listings/recordSwipe").post(function (req, res) {
-  let db_connect = dbo.getDb("sample_airbnb");
-  let myobj = {
-   listing_id : req.body.id,
-   last_modified : new Date(),
-   session_id : req.body.session_id,
-   direction : req.body.direction
-  };
-  db_connect.collection("matches").insertOne(myobj, function (err, res) {
-    if (err)  { res.status(400); 
-      res.send("Error insert matches!"); }
-  });
+  // Insert swipe informations
 });
 
 // This section will help you update a record by id.
 recordRoutes.route("/listings/updateLike").post(function (req, res) {
-  let db_connect = dbo.getDb("sample_airbnb");
-  let myquery = { _id: req.body.id };
-  let newvalues = {
-    $inc: {
-      likes : 1
-    }
-  };
-  db_connect
-    .collection("listingsAndReviews")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err)  { res.status(400); 
-        res.send("Error insert matches!"); }
-    else
-    {
-      console.log("1 document updated");
-    }
-    });
+  // Update likes
 });
 
 // This section will help you delete a record
 recordRoutes.route("/listings/delete").delete((req, res) => {
-  let db_connect = dbo.getDb("sample_airbnb");
-  var myquery = { listing_id: req.body.id };
-  db_connect.collection("matches").deleteOne(myquery, function (err, obj) {
-  if (err)  { res.status(400); 
-      res.send("Error insert matches!"); }
-  else
-  {
-    console.log("1 document deleted");
-  }
-  });
+  // Delete documents
 });
 
 module.exports = recordRoutes;
