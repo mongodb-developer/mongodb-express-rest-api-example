30aae3ba215a2752ced1db1c311b1243dfb0bc6e
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2021-08-30 11:25:08 +0300

    refactor: update front-end styling and formatting

--- app/listings/src/App.js
+++ app/listings/src/App.js
@@ -1,87 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
 import TinderCard from 'react-tinder-card'
 import axios from 'axios';
 import { Component } from 'react';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
-
-class   App extends Component
-{
+class App extends Component {
   constructor() {
     super();
-    this.state = { data: [],
-    session_id : uuid(),
-  liked : false  };
-  this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      data: [],
+      session_id: uuid(),
+      liked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.showDetails = this.showDetails.bind(this);
   }
-  
- async onSwipe(direction, listing_id, session_id){ 
-  this.setState({
-    liked: false
-  });
-  
 
-  if (direction === "left" )
-  {
-    await axios.delete(`http://localhost:5000/listings/delete/${listing_id}`)
+  async onSwipe(direction, listingId, sessionId) {
+    this.setState({
+      liked: false
+    });
+
+
+    if (direction === "left") {
+      await axios.delete(`http://localhost:5000/listings/delete/${listingId}`)
+    } else {
+      await axios.post("http://localhost:5000/listings/recordSwipe", { id: listingId, session_id: sessionId, direction })
+    }
   }
-  else
-  {
-    await axios.post("http://localhost:5000/listings/recordSwipe", {id : listing_id, session_id : session_id, direction : direction})
+
+  async handleClick(listingId) {
+    this.setState({
+      liked: !this.state.liked
+    });
+
+    await axios.post("http://localhost:5000/listings/updateLike", { id: listingId });
   }
-}
 
-async handleClick(listing_id) {
-  this.setState({
-    liked: !this.state.liked
-  });
-  await axios.post("http://localhost:5000/listings/updateLike", {id : listing_id})
-  
+  showDetails(listing) {
+    alert(`Name: ${listing.name}\n Price : $${listing.price['$numberDecimal']} \n Minimum Nights : ${listing.minimum_nights}\n Beds : ${listing.beds}`);
+  }
 
-}
+  async componentWillMount() {
+    const response = await axios.get(`http://localhost:5000/listings`);
+    const json = await response.data;
+    this.setState({ data: json });
+  }
 
+  render() {
+    const likeButtonLabel = this.state.liked ? '❤' : 'Like';
 
-showDetails(listing)
-{
-  alert(`Name: ${listing.name}\n Price : $${listing.price['$numberDecimal']} \n Minimum Nights : ${listing.minimum_nights}\n Beds : ${listing.beds}`)
-}
+    return (
+        <div className="app">
+          <div>
+            <h1>LisTinder</h1>
+            <h2>Swipe left for drop or right to save...</h2>
 
-async componentWillMount() {
-  const response = await axios.get(`http://localhost:5000/listings`);
-  const json = await response.data;
-  this.setState({ data: json });
-}
-
-
-
-render(){
-  const label = this.state.liked ? '❤' : 'Like'
-  return (
-    <div>
-    <div className="App">
-      <h1>LisTinder</h1>
-      <h2> Swipe left for drop or right to save...</h2>
-      {this.state.data.map((listing) =>
-          <TinderCard className='swipe' key={listing.name} onSwipe={(dir) => this.onSwipe(dir, listing._id)}  >
-            <div style={{ backgroundImage: 'url(' + listing.images.picture_url + ')' }} className='card' onDoubleClick={() => this.showDetails(listing)}>
-              <h3>{listing.name}
-              <div className="customContainer">
-        <button className="btn btn-primary" onClick={() => this.handleClick(listing._id)}>
-          {label}</button>
+            <div className="card-container">
+            {this.state.data.map((listing) =>
+              <TinderCard className='swipe' key={listing.name} onSwipe={(dir) => this.onSwipe(dir, listing._id)}  >
+                <div style={{ backgroundImage: 'url(' + listing.images.picture_url + ')' }} className='card'>
+                  <div className="card-details">
+                    <h3>{listing.name}</h3>
+                    <div className="card-actions">
+                      <button className="button" onClick={() => this.handleClick(listing._id)}>{likeButtonLabel}</button>
+                      <button className="button" onClick={() => this.showDetails(listing)}>See Details</button>
+                    </div>
+                  </div>
+                </div>
+              </TinderCard>
+            )}
             </div>
-              </h3>
-              Double click for details
-            </div>
-            
-          </TinderCard> )}
-          
-     
-    </div>
-   
-   </div>
-  );
-}
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
