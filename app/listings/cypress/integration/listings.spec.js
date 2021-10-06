2120a3880f399fed3ca73f6882f2d240f2756346
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2021-10-06 20:24:11 +0300

    chore: run prettier on cypress tests

--- app/listings/cypress/integration/listings.spec.js
+++ app/listings/cypress/integration/listings.spec.js
@@ -5,17 +5,16 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 describe('Web site availability', () => {
-    it('Sanity listings web site', () => {
-      cy.visit('http://localhost:3000')
-      cy.contains("LisTinder").should('exist')
-      cy.contains("Like").should('exist')
-
-    })
-    it('Test Like listings', () => {
-        cy.visit('http://localhost:3000')
-        cy.contains("Like").click({force : true})
-        cy.contains("❤").should('exist')
-        cy.contains("❤").click({force : true})
-        cy.contains("❤").should('not.exist')
-    })
-  })
+  it('Sanity listings web site', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('LisTinder').should('exist');
+    cy.contains('Like').should('exist');
+  });
+  it('Test Like listings', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('Like').click({ force: true });
+    cy.contains('❤').should('exist');
+    cy.contains('❤').click({ force: true });
+    cy.contains('❤').should('not.exist');
+  });
+});
