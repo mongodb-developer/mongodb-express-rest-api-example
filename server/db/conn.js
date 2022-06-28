ea1cc1491b01085c71ebdf88e5f27b9de2a07578
Anthony <anthonyc.animba@gmail.com>
2022-06-28 13:10:25 +0100

    feat: setup

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,4 +1,3 @@
-
 const { MongoClient } = require("mongodb");
 const connectionString = process.env.ATLAS_URI;
 const client = new MongoClient(connectionString, {
@@ -10,7 +9,16 @@ let dbConnection;
 
 module.exports = {
   connectToServer: function (callback) {
-    // Implement Database connection
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("sample_airbnb");
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
+    });
   },
 
   getDb: function () {
